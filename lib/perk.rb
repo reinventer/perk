34dd6830e2721b3124dc36279e9875bb2a32a55a
Dmitry Panov <reinventer@gmail.com>
2016-03-18 16:50:09 +0300

    clean visits method

--- lib/perk.rb
+++ lib/perk.rb
@@ -9,5 +9,9 @@ module Perk
       Visit.where("visits.created_at between ? and ?", start_time, end_time).
         includes(*%i(session ip url_domain url_path url_query_string referer_domain referer_path referer_query_string user_agent))
     end
+
+    def clean(end_time)
+      Visit.where("visits.created_at < ?", end_time).delete_all
+    end
   end
 end
